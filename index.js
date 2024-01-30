d6c9bcbb1a6090a3d3dab196725c55d37f47ec7f
Vijay Chouhan <vijaychouhan.rails@gmail.com>
2024-01-30 14:39:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,9 +13,10 @@ const {
   InteractionManager,
 } = ReactNative;
 
-const ViewPagerAndroid = require('@react-native-community/viewpager');
+// const ViewPagerAndroid = require('@react-native-community/viewpager');
 const TimerMixin = require('react-timer-mixin');
-const ViewPager = require('@react-native-community/viewpager');
+// const ViewPager = require('@react-native-community/viewpager');
+import PagerView from 'react-native-pager-view';
 
 const SceneComponent = require('./SceneComponent');
 const DefaultTabBar = require('./DefaultTabBar');
